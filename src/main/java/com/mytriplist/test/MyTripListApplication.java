263c20339b01374b14de455fa3b65b2e6b156aa6
P-chaewon <100437569+chaechae1s@users.noreply.github.com>
2022-03-17 01:02:23 +0900

    Initial commit

---
+++ src/main/java/com/mytriplist/test/MyTripListApplication.java
@@ -0,0 +1,13 @@
+package com.mytriplist.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTripListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTripListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mytriplist/test/MyTripListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytriplist.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTripListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
